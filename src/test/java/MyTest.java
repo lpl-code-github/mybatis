cf92c7f92aeed9b854c91470ba2e2a85b0b33b53
李鹏龙 <819508292@qq.com>
2022-01-19 23:32:47 +0800

    查询全部测试

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,30 @@
+import com.lpl.mybatis.config.MybatisConfig;
+import com.lpl.mybatis.domain.User;
+import com.lpl.mybatis.mapper.UserMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+/**
+ * @author lipenglong
+ * @version 1.0
+ * @describe
+ */
+public class MyTest {
+    @Test
+    public void select(){
+        SqlSession session = MybatisConfig.getSession();
+        // 方法一
+        // List<User> list = session.selectList("com.lpl.mybatis.mapper.UserMapper.selectUser");
+        // 方法二
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        List<User> list = mapper.selectUser();
+        // 打印
+        for (User user: list) {
+            System.out.println(user);
+        }
+        // 关闭连接
+        session.close();
+    }
+}

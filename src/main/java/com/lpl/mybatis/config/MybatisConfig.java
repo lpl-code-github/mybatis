51e722369ebf0baaa695b29b38c3f278ea621c1e
李鹏龙 <819508292@qq.com>
2022-01-19 23:30:41 +0800

    mybatis的配置

---
+++ src/main/java/com/lpl/mybatis/config/MybatisConfig.java
@@ -0,0 +1,35 @@
+package com.lpl.mybatis.config;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+/**
+ * @author lipenglong
+ * @version 1.0
+ * @describe
+ */
+
+public class MybatisConfig {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        try{
+            String resource = "config/MybatisConfig.xml";
+            InputStream resourceAsStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 获取SqlSession连接
+    public static SqlSession getSession(){
+        return sqlSessionFactory.openSession();
+    }
+}
